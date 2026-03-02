de9a158dd5ae5b5eb29533d633a1e5706d10b827
FX Nicolas <francoisxavier.nicolas@gmail.com>
2026-03-02 16:49:44 +0100

    Fix Cleaning the mess in the preview class.

--- src/extension.ts
+++ src/extension.ts
@@ -8,35 +8,17 @@ import { getConfig } from './utils';
 
 export function activate(context: vscode.ExtensionContext) {
 	let preview = new Preview(context);
-	let disposableSidePreview = vscode.commands.registerCommand('homebrewery4vsc.sidePreview', async () => {
-		await preview.initMarkdownPreview(vscode.ViewColumn.Two);
-	});
+	
+	let disposableSidePreview = vscode.commands.registerCommand('homebrewery4vsc.sidePreview', async () => { await preview.initMarkdownPreview(vscode.ViewColumn.Two); });
+	let disposableStandalonePreview = vscode.commands.registerCommand('homebrewery4vsc.preview', async () => { await preview.initMarkdownPreview(vscode.ViewColumn.One); });
 
-	let disposableStandalonePreview = vscode.commands.registerCommand('homebrewery4vsc.preview', async () => {
-		await preview.initMarkdownPreview(vscode.ViewColumn.One);
-	});
-
-	let generateCommand = vscode.commands.registerCommand('homebrewery4vsc.generate', () => {
-		generateFile(context);
-	});
-	let previewLayoutSimpleSpread = vscode.commands.registerCommand('homebrewery4vsc.previewLayoutSimpleSpread', () => {
-		preview.togglePreviewLayoutSpread();
-	});
-	let previewLayoutFacingSpread = vscode.commands.registerCommand('homebrewery4vsc.previewLayoutFacingSpread', () => {
-		preview.togglePreviewLayoutSpread();
-	});
-	let previewLayoutFlowSpread = vscode.commands.registerCommand('homebrewery4vsc.previewLayoutFlowSpread', () => {
-		preview.togglePreviewLayoutSpread();
-	});
-	let previewZoomIn = vscode.commands.registerCommand('homebrewery4vsc.previewZoomOut', () => {
-		preview.previewZoomOut();
-	});
-	let previewZoomOut = vscode.commands.registerCommand('homebrewery4vsc.previewZoomIn', () => {
-		preview.previewZoomIn();
-	});
-	let previewZoomReset = vscode.commands.registerCommand('homebrewery4vsc.previewZoomReset', () => {
-		preview.previewZoomReset();
-	});
+	let generateCommand = vscode.commands.registerCommand('homebrewery4vsc.generate', () => { generateFile(context); });
+	let previewLayoutSimpleSpread = vscode.commands.registerCommand('homebrewery4vsc.previewLayoutSimpleSpread', () => { preview.togglePreviewLayoutSpread(); });
+	let previewLayoutFacingSpread = vscode.commands.registerCommand('homebrewery4vsc.previewLayoutFacingSpread', () => { preview.togglePreviewLayoutSpread(); });
+	let previewLayoutFlowSpread = vscode.commands.registerCommand('homebrewery4vsc.previewLayoutFlowSpread', () => { preview.togglePreviewLayoutSpread(); });
+	let previewZoomIn = vscode.commands.registerCommand('homebrewery4vsc.previewZoomOut', () => { preview.previewZoomOut(); });
+	let previewZoomOut = vscode.commands.registerCommand('homebrewery4vsc.previewZoomIn', () => { preview.previewZoomIn(); });
+	let previewZoomReset = vscode.commands.registerCommand('homebrewery4vsc.previewZoomReset', () => { preview.previewZoomReset(); });
 
 	// push to subscriptions list so that they are disposed automatically
 	context.subscriptions.push(disposableSidePreview);
--- src/preview.ts
+++ src/preview.ts
@@ -16,76 +16,24 @@ const enum LayoutSpread {
 };
 
 export default class Preview {
-
     panel: vscode.WebviewPanel | undefined;
-    editor: any;
-    line: number | undefined;
     currentLayoutSpread: LayoutSpread = LayoutSpread.Simple;
     currentZoom: number = 100;
-    // disableWebViewStyling: boolean;
     context: vscode.ExtensionContext;
-    private _resource: vscode.Uri | undefined;
-    private readonly disposables: vscode.Disposable[] = [];
-    private _disposed: boolean = false;
-    private readonly _onDisposeEmitter = new vscode.EventEmitter<void>();
-    public readonly onDispose = this._onDisposeEmitter.event;
-    private readonly _onDidChangeViewStateEmitter = new vscode.EventEmitter<vscode.WebviewPanelOnDidChangeViewStateEvent>();
+    private documentUri: vscode.Uri | undefined;
+    private isDisposed: boolean = false;
 
-    //returns true if an html document is open
     constructor(context: vscode.ExtensionContext) {
         this.context = context;
     };
 
-    async refresh() {
-        const editor = vscode.window.activeTextEditor;
-        //FIXME: ActiveTextEditor seems unset on a refresh. 
-        //FIXME: WebView is Disposed Error occurs frequently
-        //FIXME: Previewers are all refreshing from the same text editor
-        if (!editor) {
-            vscode.window.showWarningMessage(constants.ErrorMessages.NO_ACTIVE_EDITOR);
-            return;
-        }
-        if (editor && this.checkDocumentIsMarkdown(true) && this.panel) {
-            let currentMarkdownText = editor.document.getText();
-            const filePaths = editor.document.fileName.split('/');
-            const fileName = filePaths[filePaths.length - 1];
-            this.panel.title = `[Preview] ${fileName}`;
-            let currentHTMLContent = await renderHTML(currentMarkdownText, this.context, true);
-            this._resource = editor.document.uri;
-            this.panel.webview.html = currentHTMLContent;
-            this.updateZoomLevel();
-            if (editor.document.languageId === 'markdown' && getConfig().get('scrollPreviewWithEditor')) {
-                this.postMessage({
-                    type: 'scroll',
-                    page: this.computePageNumber(editor.visibleRanges, editor.document),
-                    mode: 'instant'
-                });
-            }
-        }
+    private getEditorFileName(editor: vscode.TextEditor): string {
+        const filePaths = editor.document.fileName.split('/');
+        const fileName = filePaths[filePaths.length - 1];
+        return fileName;
     }
 
-    /*
-    getWebviewContent(html: string, fileName: string) {
-         const filePaths = fileName.split('/');
-        fileName = filePaths[filePaths.length - 1];
-        const reg = /<img src\s*=\s*"(.+?)"/g;
-        var m;
-        do {
-            m = reg.exec(html);
-            if (m) {
-                let imagePath = m[1].split('/');
-                let imageName = imagePath[imagePath.length - 1];
-                let vsCodeImagePath = this.getDynamicContentPath(imageName);
-                if (vsCodeImagePath) {
-                    html = html?.replace(m[0], m[0].replace(m[1], vsCodeImagePath.toString()));
-                }
-            }
-        } while (m);
-        return renderHTML(html, this.context)
-    }
-         */
-
-    computePageNumber(visibleRanges: readonly vscode.Range[], document: vscode.TextDocument): number {
+    private computePageNumber(visibleRanges: readonly vscode.Range[], document: vscode.TextDocument): number {
         let topLine = 0;
         if (visibleRanges.length > 0) {
             topLine = visibleRanges[0].start.line;
@@ -107,101 +55,119 @@ export default class Preview {
         return pageDirectivesBefore + 1;
     }
 
-    getDynamicContentPath(filepath: string) {
-        const rootPath = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
-        if (!rootPath) {
-            return filepath;
-        }
-        const onDiskPath = vscode.Uri.file(path.join(rootPath, 'content/media', filepath));
-        if (this.panel) {
-            const styleSrc = this.panel.webview.asWebviewUri(onDiskPath);
-            return styleSrc;
-        }
-    }
-
-    getDocumentType(): string {
-        if (vscode.window.activeTextEditor) {
-            let languageId = vscode.window.activeTextEditor.document.languageId.toLowerCase();
-            return languageId;
-        } else { return ""; }
-    }
-
-    checkDocumentIsMarkdown(showWarning: boolean): boolean {
-        let result = this.getDocumentType() === "markdown";
+    private isMarkdownEditor(editor: vscode.TextEditor, showWarning: boolean = true): boolean {
+        const languageId: string = editor ? editor.document.languageId.toLowerCase() : "";
+        const result = languageId === "markdown";
         if (!result && showWarning) {
             vscode.window.showInformationMessage(constants.ErrorMessages.NOT_MARKDOWN);
         }
         return result;
     }
 
+    private initializeLayout() {
+        // Setting the Page layout
+        this.currentLayoutSpread = LayoutSpread.Simple;
+        vscode.commands.executeCommand(
+            'setContext',
+            'homebrewery.currentLayoutSpread',
+            this.currentLayoutSpread
+        );
+        // Setting the Zoop Level
+        this.currentZoom = 100;
+        vscode.commands.executeCommand(
+            'setContext',
+            'homebrewery.currentZoom',
+            this.currentZoom
+        );
+    }
+
     async initMarkdownPreview(viewColumn: number) {
-        let proceed = this.checkDocumentIsMarkdown(true);
-        if (vscode.window.activeTextEditor && proceed) {
-            const filePaths = vscode.window.activeTextEditor.document.fileName.split('/');
-            const fileName = filePaths[filePaths.length - 1];
-            // Setting the Page layout
-            this.currentLayoutSpread = LayoutSpread.Simple;
-            vscode.commands.executeCommand(
-                'setContext',
-                'homebrewery.currentLayoutSpread',
-                this.currentLayoutSpread
-            );
-            this.currentZoom = 100;
-            vscode.commands.executeCommand(
-                'setContext',
-                'homebrewery.currentZoom',
-                this.currentZoom
-            );
+        const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+        if (editor && this.isMarkdownEditor(editor)) {
+            if (this.panel) {
+                // Reuse existing panel.
+                this.documentUri = editor.document.uri;
+                await this.refresh.call(this);
+                this.panel.reveal();
+            } else {
+                // Create and show a new webview
+                this.initializeLayout();
+                this.panel = vscode.window.createWebviewPanel(
+                    'HomebrewPreview',
+                    '[Preview] ' + this.getEditorFileName(editor),
+                    viewColumn,
+                    {
+                        enableScripts: true,
+                        retainContextWhenHidden: true,
+                    }
+                );
+                // Set the preview properties
+                this.panel.iconPath = this.iconPath;
+                this.isDisposed = false;
+                this.documentUri = editor.document.uri;
 
-            // Create and show a new webview
-            this.panel = vscode.window.createWebviewPanel(
-                'HomebrewPreview',
-                '[Preview] ' + fileName,
-                viewColumn,
-                {
-                    // Enable scripts in the webview
-                    enableScripts: true,
-                    retainContextWhenHidden: true,
-                    // And restrict the webview to only loading content from our extension's `assets` directory.
-                    // localResourceRoots: [vscode.Uri.file(path.join(this.context.extensionPath, 'assets')), vscode.Uri.file(path.join(vscode.workspace.rootPath, 'content/media'))]
-                    // localResourceRoots: [vscode.Uri.file(path.join(this.context.extensionPath, 'assets'))]
-                }
-            );
-            this.panel.iconPath = this.iconPath;
-            this._disposed = false;
+                // And set its HTML content
+                await this.refresh.call(this);
 
-            // And set its HTML content
-            this.editor = vscode.window.activeTextEditor;
-            await this.refresh.call(this);
+                // Register events for refresh
+                vscode.workspace.onDidChangeTextDocument(await this.refresh.bind(this));
+                vscode.workspace.onDidChangeConfiguration(await this.refresh.bind(this));
+                vscode.workspace.onDidSaveTextDocument(await this.refresh.bind(this));
+                vscode.window.onDidChangeActiveTextEditor(await this.refresh.bind(this));
 
-            vscode.workspace.onDidChangeTextDocument(await this.refresh.bind(this));
-            vscode.workspace.onDidChangeConfiguration(await this.refresh.bind(this));
-            vscode.workspace.onDidSaveTextDocument(await this.refresh.bind(this));
-            vscode.window.onDidChangeActiveTextEditor(await this.refresh.bind(this));
+                // Process editor scroll events
+                vscode.window.onDidChangeTextEditorVisibleRanges(({ textEditor, visibleRanges }) => {
+                    if (this.isMarkdownEditor(textEditor) && getConfig().get('scrollPreviewWithEditor')) {
+                        this.postMessage({
+                            type: 'scroll',
+                            page: this.computePageNumber(visibleRanges, textEditor.document),
+                            mode: 'smooth'
+                        });
+                    }
+                });
 
-            vscode.window.onDidChangeTextEditorVisibleRanges(({ textEditor, visibleRanges }) => {
-                if (textEditor.document.languageId === 'markdown' && getConfig().get('scrollPreviewWithEditor')) {
-                    this.postMessage({
-                        type: 'scroll',
-                        page: this.computePageNumber(visibleRanges, textEditor.document),
-                        mode: 'smooth'
-                    });
-                }
-            });
+                // Webview scroll events
+                this.panel.webview.onDidReceiveMessage(message => {
+                    // Clicking in the webview sends a message { "goToPage", targetPage }
+                    if (message.type === 'goToPage') {
+                        this.scrollEditorToPage(message.page);
+                    }
+                });
 
-            // Process incoming messages from the webview.
-            this.panel.webview.onDidReceiveMessage(message => {
-                // Clicking in the webview sends a message { "goToPage", targetPage }
-                if (message.type === 'goToPage') {
-                    this.scrollEditorToPage(message.page);
-                }
-            });
-
-            this.panel.onDidDispose(() => {
-                this.dispose();
-            }, null, this.disposables);
+                // Panel Disposal
+                this.panel.onDidDispose(() => {
+                    this.isDisposed = true;
+                    this.panel = undefined;
+                }, null, this.context.subscriptions);
+            }
         }
     };
+
+    async refresh() {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            // FIXME: Switching text causes no Active Text Editor (SUPPRESSED)
+            // vscode.window.showWarningMessage(constants.ErrorMessages.NO_ACTIVE_EDITOR);
+            return;
+        }
+        if (editor && this.isMarkdownEditor(editor, true) && this.panel) {
+            let currentMarkdownText = editor.document.getText();
+            this.panel.title = `[Preview] ${this.getEditorFileName(editor)}`;
+            let currentHTMLContent = await renderHTML(currentMarkdownText, this.context, true);
+            this.panel.webview.html = currentHTMLContent;
+            this.documentUri = editor.document.uri;
+            this.updateZoomLevel();
+
+            // FIXME: Only scroll if active text editor is changed
+            if (editor.document.languageId === 'markdown' && getConfig().get('scrollPreviewWithEditor')) {
+                this.postMessage({
+                    type: 'scroll',
+                    page: this.computePageNumber(editor.visibleRanges, editor.document),
+                    mode: 'instant'
+                });
+            }
+        }
+    }
 
     private scrollEditorToPage(targetPage: number) {
         // Synchronize Editor scroll with preview.
@@ -254,11 +220,9 @@ export default class Preview {
             case LayoutSpread.Simple:
                 this.currentLayoutSpread = LayoutSpread.Facing;
                 break;
-
             case LayoutSpread.Facing:
                 this.currentLayoutSpread = LayoutSpread.Flow;
                 break;
-
             case LayoutSpread.Flow:
                 this.currentLayoutSpread = LayoutSpread.Simple;
                 break;
@@ -312,8 +276,8 @@ export default class Preview {
     };
 
     private isPreviewOf(resource: vscode.Uri): boolean {
-        if (this._resource) {
-            return (this._resource.fsPath === resource.fsPath);
+        if (this.documentUri) {
+            return (this.documentUri.fsPath === resource.fsPath);
         }
         else {
             return false;
@@ -329,33 +293,8 @@ export default class Preview {
     }
 
     private postMessage(msg: any) {
-        if (this.panel && !this._disposed) {
+        if (this.panel && !this.isDisposed) {
             this.panel.webview.postMessage(msg);
         }
-    }
-
-    private disposeAll(disposables: vscode.Disposable[]) {
-        while (disposables.length) {
-            const item = disposables.pop();
-            if (item) {
-                item.dispose();
-            }
-        }
-    }
-
-    public dispose() {
-        if (this._disposed) {
-            return;
-        }
-
-        this._disposed = true;
-        this._onDisposeEmitter.fire();
-
-        this._onDisposeEmitter.dispose();
-        if (this.panel) {
-            this.panel.dispose();
-        }
-
-        this.disposeAll(this.disposables);
     }
 }
